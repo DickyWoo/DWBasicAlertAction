0f48990d0eb3317a844a5e8b841bb0240d323bd4
wujiandongxq <759319223@qq.com>
2017-08-21 19:17:15 +0800

    优化README

--- DWBasicAlertActionDemo/DWBasicAlertActionDemo/ViewController.m
+++ DWBasicAlertActionDemo/DWBasicAlertActionDemo/ViewController.m
@@ -22,16 +22,21 @@
 }
 
 - (IBAction)alertButtonClick:(id)sender {
+    
     [DWBasicAlertAction showAlertWithTitle:@"Alert" message:@"DWBasicAlertAction" cancelButtonTitle:@"cancel" otherButtonTitles:@[@"item1",@"item2",@"item3",@"item4",@"item5",@"item6"]  chooseBlock:^(NSInteger buttonIdx) {
         NSLog(@"click buttonIdx:%ld",buttonIdx);
     }];
+    
 }
 
 - (IBAction)actionSheetButtonClick:(id)sender {
+    
     [DWBasicAlertAction showActionSheetWithTitle:@"ActionSheet" message:@"DWBasicAlertAction" cancelButtonTitle:@"Cancel" destructiveButtonTitle:@"Destrutive" otherButtonTitles:@[@"item1",@"item2",@"item3",@"item4",@"item5",@"item6"] chooseBlock:^(NSInteger buttonIdx) {
         NSLog(@"click buttonIdx:%ld",buttonIdx);
     }];
+    
 }
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
